f53a5cd46fa2c570989b128bcd98c3aadab2d1f0
Sir Ubu <thesirubu@gmail.com>
2021-03-23 18:16:34 -0600

    Updated layout of initial game logic.

--- assets/js/game.js
+++ assets/js/game.js
@@ -18,46 +18,12 @@ var enemyAttack = 10;
 // fight function
 var fight = function(enemyName) {
     // repeat and execute while enemy-robot is alive
-    while (enemyHealth > 0) {
+    while (enemyHealth > 0 && playerHealth > 0) {
         // ask if player wants to fight or skip
         var promptFight = prompt("Would you like to FIGHT or SKIP this battle? \nEnter 'FIGHT' to fight. \nEnter 'SKIP' to skip.");
-
-        console.log(promptFight);
-
-        // if fight, run round
-        if (promptFight === "fight" || promptFight === "FIGHT") {
-            // alert fight start
-            alert("I like your fighting style!")
-
-            // subtract the value of `playerAttack` from the value of `enemyHealth` and update `enemyHealth` variable
-            enemyHealth = enemyHealth - playerAttack;
-
-            // log a resulting message to the console that it worked
-            console.log(`${playerName} attacked ${enemyName} and dealt ${playerAttack} damage. ${enemyName} now has ${enemyHealth} health remaining.`);
-
-            // check enemy's health
-            if (enemyHealth <= 0) {
-                alert(`${enemyName} had been defeated!`);
-            } else {
-                alert(`${enemyName} still has ${enemyHealth} health left.`)
-            }
-
-            // subtract the value of `enemyAttack` from the value of `playerHealth` and update `playerHealth` variable
-            playerHealth = playerHealth - enemyAttack;
-
-            // log a resulting message to the console that it worked
-            console.log(`${enemyName} attacked ${playerName} and dealt ${enemyAttack} damage. ${playerName} now has ${playerHealth} health remaining.`);
-
-            // check player's health
-            if (playerHealth <= 0) {
-                alert(`${playerName} had been defeated!`);
-            } else {
-                alert(`${playerName} still has ${playerHealth} health left.`)
-            }
         
-        } 
-        // else if skip, cost player and end round
-        else if (promptFight === "skip" || promptFight === "SKIP") {
+        // if skip, cost player and end round
+        if (promptFight === "skip" || promptFight === "SKIP") {
             // confirm player wants to skip
             var confirmSkip = confirm("You will be penalized for skipping. \nAre you sure you want to skip?");
 
@@ -65,20 +31,38 @@ var fight = function(enemyName) {
             if (confirmSkip) {
                 alert(`This is embarrassing... ${playerName} has decided to skip this fight.`)
 
+                // reduce player score for skipping
                 playerScore = playerScore - 2;
+                break;
             }
-            // if no, ask question again
-            else {
-                fight();
-            }
-            
-        
-        } 
-        // incorrect input, restart
-        else {
-            alert("You need to choose a valid option. Try again!")
+        }
 
-            fight();
+        // subtract the value of `playerAttack` from the value of `enemyHealth` and update `enemyHealth` variable
+        enemyHealth = enemyHealth - playerAttack;
+
+        // log a resulting message to the console that it worked
+        console.log(`${playerName} attacked ${enemyName} and dealt ${playerAttack} damage. ${enemyName} now has ${enemyHealth} health remaining.`);
+
+        // check enemy's health
+        if (enemyHealth <= 0) {
+            alert(`${enemyName} had been defeated!`);
+            break;
+        } else {
+            alert(`${enemyName} still has ${enemyHealth} health left.`)
+        }
+
+        // subtract the value of `enemyAttack` from the value of `playerHealth` and update `playerHealth` variable
+        playerHealth = playerHealth - enemyAttack;
+
+        // log a resulting message to the console that it worked
+        console.log(`${enemyName} attacked ${playerName} and dealt ${enemyAttack} damage. ${playerName} now has ${playerHealth} health remaining.`);
+
+        // check player's health
+        if (playerHealth <= 0) {
+            alert(`${playerName} had been defeated!`);
+            break;
+        } else {
+            alert(`${playerName} still has ${playerHealth} health left.`)
         }
     }
 };
